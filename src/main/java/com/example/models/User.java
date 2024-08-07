d3b582cc1ab777b53736196f1c29581b858718d9
sutikshan-sikarwar <sutikshan.sikarwar@gmail.com>
2024-08-08 00:29:51 +0530

    Your commit message

---
+++ src/main/java/com/example/models/User.java
@@ -0,0 +1,139 @@
+package com.example.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+    
+    private List<Integer> followers = new ArrayList<>();
+    
+    private List<Integer> following = new ArrayList<>();
+    
+    @JsonIgnore
+    @ManyToMany
+    private List<Post> savedPost = new ArrayList<>();
+    
+    public List<Post> getSavedPost() {
+		return savedPost;
+	}
+
+
+
+	public void setSavedPost(List<Post> savedPost) {
+		this.savedPost = savedPost;
+	}
+
+
+	private String gender;
+    
+    public User() {
+        // Default constructor
+    }
+
+    
+
+    public User(Integer id, String firstName, String lastName, String email, String password, List<Integer> followers,
+			List<Integer> following, String gender) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.followers = followers;
+		this.following = following;
+		this.gender = gender;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+	public List<Integer> getFollowers() {
+		return followers;
+	}
+
+
+	public void setFollowers(List<Integer> followers) {
+		this.followers = followers;
+	}
+
+
+	public List<Integer> getFollowing() {
+		return following;
+	}
+
+
+	public void setFollowing(List<Integer> following) {
+		this.following = following;
+	}
+}
